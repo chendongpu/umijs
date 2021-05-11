89d790910dfbe574c383539afee60ff30d1cd4c9
chendongpu <2323178881@qq.com>
2021-05-11 15:11:18 +0800

    配置一个user页面

--- .umirc.ts
+++ .umirc.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     },
   routes: [
     { path: '/', component: '@/pages/index',title:'首页' },
+    { path: '/user', component: '@/pages/user',title:'用户页面' },
   ],
   fastRefresh: {},
 });
---
+++ src/pages/user.tsx
@@ -0,0 +1,10 @@
+
+import styles from './index.less';
+
+export default function UserPage() {
+    return (
+        <div>
+            <h1 className={styles.title}>Page user</h1>
+        </div>
+    );
+}
