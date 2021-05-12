7e4cf130eff936897c62d333162e19b2a5bc892f
chendongpu <2323178881@qq.com>
2021-05-12 09:57:54 +0800

    使用props的history跳转页面

--- src/pages/one.tsx
+++ src/pages/one.tsx
@@ -1,9 +1,15 @@
 
 import styles from './index.less';
+import {Button} from 'antd';
 
-export default function OnePage() {
+export default function OnePage(props:any) {
+
+    console.log("props:",props);
     return (
         <div>
+            <Button onClick={()=>{
+                props.history.push('/');
+            }}>回到首页</Button>
             <h1 className={styles.title}>Page one</h1>
         </div>
     );
