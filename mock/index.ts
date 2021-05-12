aaf10135ca4b7178a4c432fd4d9028191ad7042f
chendongpu <2323178881@qq.com>
2021-05-12 10:36:29 +0800

    请求mock数据

---
+++ mock/index.ts
@@ -0,0 +1,12 @@
+export default {
+    'GET /api/index':{
+        id:1,
+        name:'Tom',
+        age:12
+    },
+    'POST /api/person':{
+        id:1,
+        name:'Lily',
+        age:12
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import styles from './index.less';
-import {DatePicker} from 'antd';
+import {DatePicker,Button} from 'antd';
 import {useEffect} from 'react';
-import {history} from 'umi';
+import {history,request} from 'umi';
 
 export default function IndexPage() {
     useEffect(()=>{
-        setTimeout(()=>{
-            history.push('/sub/one');
-        },2000)
-    },[])
+        // setTimeout(()=>{
+        //     history.push('/sub/one');
+        // },2000)
+    },[]);
+
+    const getData=()=>{
+        //请求数据
+        request('/api/index').then((res)=>{
+            console.log(res);
+        })
+    };
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+        <Button onClick={getData}>获取状态数据</Button>
         <DatePicker />
     </div>
   );
