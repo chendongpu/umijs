3386da01df7f2b651dd4292b6373f61e75f700b6
chendongpu <2323178881@qq.com>
2021-05-12 09:51:12 +0800

    history跳转页面

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import styles from './index.less';
-import {DatePicker} from 'antd'
+import {DatePicker} from 'antd';
+import {useEffect} from 'react';
+import {history} from 'umi';
 
 export default function IndexPage() {
+    useEffect(()=>{
+        setTimeout(()=>{
+            history.push('/sub/one');
+        },2000)
+    },[])
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
